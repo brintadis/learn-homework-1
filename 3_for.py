1506fe6f336979a06b0b4a12761aaf00fd27a6ed
Makar Timofeev <makar.timofeev.1990@gmail.com>
2022-05-17 19:21:58 +0300

    for task done

--- 3_for.py
+++ 3_for.py
@@ -16,12 +16,25 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+
+def main(product_list):
+	all_items_sold = []
+
+	for product in product_list:
+		print(f"Cуммарное количество продаж {product['product']} - {sum(product['items_sold'])}")
+		print(f"Среднее количество продаж {product['product']} - {round(sum(product['items_sold']) / len(product['items_sold']))}")
+		all_items_sold += product['items_sold']
+
+	print(f"Cуммарное количество продаж всех товаров - {sum(all_items_sold)}")
+	print(f"Среднее количество продаж всех товаров - {round(sum(all_items_sold) / len(all_items_sold))}")
+
+product_list = [
+	{'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+    {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+	]
+
     
 if __name__ == "__main__":
-    main()
+    main(product_list)
