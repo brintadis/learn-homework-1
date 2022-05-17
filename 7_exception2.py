63a71364f6f9cb9576635dd7d83111ed17053bc6
Makar Timofeev <makar.timofeev.1990@gmail.com>
2022-05-17 20:04:01 +0300

    exc 2 done

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,11 +13,20 @@
     
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
+def discounted(price, discount, max_discount=20):
+	try:
+		price = float(price)
+		discount = float(discount)
+		max_discount = int(max_discount)
+	except (TypeError, ValueError):
+		return 'Переданы некорректные аргументы.'
+	else:
+		if max_discount >= 100:
+			raise ValueError('Слишком большая максимальная скидка')
+		if discount >= max_discount:
+			return price
+		else:
+			return price - (price * discount / 100) 
     
 if __name__ == "__main__":
     print(discounted(100, 2))
